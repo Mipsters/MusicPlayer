80845df5948d6d45fe9d320ed5d115d236721ccc
Mipster <tomaviv57@gmail.com>
2016-10-13 08:41:54 +0300

    added activity

---
+++ MusicPlayer/app/src/main/java/com/example/tom/musicplayer/MusicPlayer.java
@@ -0,0 +1,20 @@
+package com.example.tom.musicplayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by Chen on 13/10/2016.
+ */
+
+public class MusicPlayer extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+
+
+    }
+}
