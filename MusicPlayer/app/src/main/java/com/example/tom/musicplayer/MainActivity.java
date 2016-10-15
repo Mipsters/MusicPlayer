a06bd2dd2d667e691835e41c6d114ef9de850c70
Mipster <tomaviv57@gmail.com>
2016-10-15 10:16:01 +0300

    created a service

--- MusicPlayer/app/src/main/java/com/example/tom/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/tom/musicplayer/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.tom.musicplayer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        ListView listView = (ListView)findViewById(R.id.listview);
+
+        ArrayAdapter<String> arr = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        arr.add("Cockroach King");
+        arr.add("Lone Digger");
+        arr.add("Non-Stop");
+        listView.setAdapter(arr);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this,MusicIntentService.class);
+                intent.putExtra("loc",position);
+                startService(intent);
+                startActivity(new Intent(MainActivity.this,MusicPlayer.class));
+            }
+        });
     }
 }
--- MusicPlayer/app/src/main/java/com/example/tom/musicplayer/MusicPlayer.java
+++ MusicPlayer/app/src/main/java/com/example/tom/musicplayer/MusicPlayer.java
@@ -1,8 +1,12 @@
 package com.example.tom.musicplayer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Chen on 13/10/2016.
@@ -10,11 +14,79 @@ import android.widget.TextView;
 
 public class MusicPlayer extends AppCompatActivity {
 
+    private int location;
+    public static MediaPlayer mediaPlayer;
+    private TextView title;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
 
+        Button prev = (Button)findViewById(R.id.prev);
+        Button st_ps = (Button)findViewById(R.id.st_ps);
+        Button nxt = (Button)findViewById(R.id.nxt);
+        title = (TextView)findViewById(R.id.textView);
 
+        /*
+        location = getIntent().getIntExtra("loc",-1);
+
+        int file = getFile(location);
+        if(file != 0)
+            mediaPlayer = MediaPlayer.create(this, file);
+        else
+            Toast.makeText(this,"problem receiving the file",Toast.LENGTH_LONG).show();
+        */
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MusicIntentService.prev.run();
+            }
+        });
+
+        st_ps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MusicIntentService.st_ps.run();
+            }
+        });
+
+        nxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MusicIntentService.next.run();
+            }
+        });
     }
+/*
+    private int getFile(int location){
+        switch (location){
+            case 0:
+                title.setText("Cockroach King");
+                return R.raw.cockroach_king;
+            case 1:
+                title.setText("Lone Digger");
+                return R.raw.lone_digger;
+            case 2:
+                title.setText("Non Stop");
+                return R.raw.non_stop;
+        }
+        return 0;
+    }
+
+    private int prev(){
+        if(location > 0)
+            return getFile(--location);
+        location = 2;
+        return getFile(location);
+    }
+
+    private int nxt(){
+        if(location < 2)
+            return getFile(++location);
+        location = 0;
+        return getFile(location);
+    }
+*/
 }
